9d1bf203b9dfa158c91b8c73bd9e98c68481e573
Tors Dalid <cletedalid@gmail.com>
2015-01-08 16:51:29 +0800

    minimal vim bundle for ruby/rails dev

---
+++ bundle/update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd ~/.vim/bundle
+for directory in `ls ~/.vim/bundle`
+do
+  echo "Checking for $directory update..."
+    if [ -d "$directory/.git" ]
+      then
+        cd $directory
+        git pull
+        cd ..
+      else
+        echo "$directory not a git plugin. Skipping update."
+     fi
+  echo
+done
+exit 0
